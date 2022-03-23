01e4c6491471dcafb92196b0087f0e02757b8cc3
dinesh nangare <dineshnangare1717@gmail.com>
2022-03-24 00:00:19 +0530

    Emp daily wage

--- employeewage.java
+++ employeewage.java
@@ -1,18 +1,27 @@
 package Day3.EmpWage;
+// UC-2 Daily Employees Wage
 import java.util.Random;
-//employee attendance
 
 public class employeewage {
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
+
     public static void main(String[] args) {
+        // Taking random function from java lib,
         Random random = new Random();
         // Taking random number 2 because of 0 for the absent and 1 for the present,
         int empPresent = random.nextInt(2);
+
+        int empWage = 0;
 
         if(empPresent == 0) {
             System.out.println("Employees Is Absent");
         }
         else {
+            empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
             System.out.println("Employees is Present");
         }
+        System.out.println("Employees Wage is" + empWage);
     }
 }
+
