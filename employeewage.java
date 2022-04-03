b0159094eb04a2f711452375dfb1e7c57b0f41a9
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-03 22:38:29 +0530

    switch case

--- employeewage.java
+++ employeewage.java
@@ -1,5 +1,5 @@
 package Day3.EmpWage;
-//UC3
+//UC4
 
 public class employeewage {
     public static void main(String[] args) {
@@ -9,16 +9,25 @@ public class employeewage {
         int IsPartTime = 2;
         int Salary = 0;
 
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IsFullTime) {
-            Salary = (EmpHr * EmpWagePerHr);
-            System.out.println("Employee salary for full time is: " + Salary);
-        } else if (empCheck == IsPartTime) {
-            EmpHr = 4;
-            Salary = (EmpHr * EmpWagePerHr);
-            System.out.println("Employee salary for part time is: " + Salary);
-        } else
-            System.out.println("Employee salary for absent is :" + Salary);
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case 0:
+                EmpHr = 8;
+                Salary = (EmpHr * EmpWagePerHr);
+                System.out.println("Employee salary for full time is: " + Salary);
+                break;
 
+            case 1:
+                EmpHr = 4;
+                Salary = (EmpHr * EmpWagePerHr);
+                System.out.println("Employee salary for part time is: " + Salary);
+                break;
+
+            case 2:
+                EmpHr = 0;
+                Salary = (EmpHr * EmpWagePerHr);
+                System.out.println("Employee salary for Absent  is: " + Salary);
+                break;
+        }
     }
 }
