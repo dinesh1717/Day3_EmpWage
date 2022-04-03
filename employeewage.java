d3dce3774bd45f1e67e1267456d37e53461cdf75
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-03 22:30:37 +0530

    daily wage condition updated

--- employeewage.java
+++ employeewage.java
@@ -1,16 +1,25 @@
 package Day3.EmpWage;
-
+import java.util.Scanner;
 //UC1
 
 public class employeewage {
-    public static void main(String[] args) {
-        int isfulltime = 1;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == isfulltime)
-            System.out.println("Employee is Present");
-        else
-            System.out.println("Employee is Absent");
+    public static void main(String[] args)
+    {
 
+        System.out.println("Enter numbers of hours worked");
+        Scanner scan = new Scanner(System.in);
+        double totalHoursWorked = scan.nextInt();
+        double standardWages = 20;
+        int standardHours = 8;
+        double totalWage;
+        if (totalHoursWorked > 8) {
+            totalWage = (standardHours * totalHoursWorked) + (totalHoursWorked - standardHours);
+        } else if (totalHoursWorked < 8) {
+            totalWage = standardWages * totalHoursWorked;
+        } else {
+            totalWage = standardWages * standardHours;
+        }
+        System.out.println("MY total wages of full day is:" + totalWage);
 
     }
 }
